81dfd2c90a7673e1f3f710128a7da39de23995ab
Raphael Santos <raphaelsantos.j@gmail.com>
2024-02-09 11:29:09 -0300

    fix: Remove uuid and timestamp columns to resolve COPY command error

--- lib/desafio_backend/trading/trade.ex
+++ lib/desafio_backend/trading/trade.ex
@@ -2,22 +2,20 @@ defmodule DesafioBackend.Trading.Trade do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @primary_key {:id, :binary_id, autogenerate: true}
-  @foreign_key_type :binary_id
   schema "trades" do
     field :hora_fechamento, :string
     field :data_negocio, :date
     field :codigo_instrumento, :string
     field :preco_negocio, :float
     field :quantidade_negociada, :integer
-
-    timestamps(type: :utc_datetime)
   end
+
+  @required_fields ~w(hora_fechamento data_negocio codigo_instrumento preco_negocio quantidade_negociada)a
 
   @doc false
   def changeset(trade, attrs) do
     trade
-    |> cast(attrs, [:hora_fechamento, :data_negocio, :codigo_instrumento, :preco_negocio, :quantidade_negociada])
-    |> validate_required([:hora_fechamento, :data_negocio, :codigo_instrumento, :preco_negocio, :quantidade_negociada])
+    |> cast(attrs, @required_fields)
+    |> validate_required(@required_fields)
   end
 end
--- priv/repo/migrations/20240209132350_create_trades.exs
+++ priv/repo/migrations/20240209132350_create_trades.exs
@@ -2,15 +2,12 @@ defmodule DesafioBackend.Repo.Migrations.CreateTrades do
   use Ecto.Migration
 
   def change do
-    create table(:trades, primary_key: false) do
-      add :id, :binary_id, primary_key: true
+    create table(:trades) do
       add :hora_fechamento, :string
       add :data_negocio, :date
       add :codigo_instrumento, :string
       add :preco_negocio, :float
       add :quantidade_negociada, :integer
-
-      timestamps(type: :utc_datetime)
     end
 
     create index(:trades, [:data_negocio, :codigo_instrumento])
